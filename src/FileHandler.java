daf19f8aa0e3dc944a68c12c71c92f075845b787
Clayton Cornell <81330047+CCGameing@users.noreply.github.com>
2024-05-24 10:04:28 -0400

    5/24/2024 - 10:03 AM
    Still having Critical parseInt Issue :/

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -25,7 +25,7 @@ public class FileHandler {
                 return null;
             }
 
-            //#region Critical Issue
+            //#region Critical Issue Here
 
             /*
             for (int i = 0; i < splitLine.length; i++) {
